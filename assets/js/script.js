52bf2c66adfa205f83e4c3e5d43595cdd7edcc5c
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-05-09 19:25:48 -0400

    application pseudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// once user opens deployed application they are presented with a decent UI
+// user inputs height, weight, age in first container .bmiInput
+// press a #calcBmi button 
+// presented with bmi results in second container .bmiRes
+// user inputs activity, weight and duration in container .calInput
+// press a #calBurn button
+// presented with calories burned based on activity
+
+
+//Fetch bmiInput API
+//RESPONSE
+//DATA
+//PRESENT THE RESULTS FROM bmiInput on page
+//store it in local storage 
+
+
+//Fetch calInuput API
+//RESPONSE
+//DATA
+//PRESENT THE RESULTS FROM bmiInput on page
+//store it in local storage 
+
+//Localstorage
+//get and set item

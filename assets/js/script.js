55e5034f553334f21295905f497f49bab661e9df
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-05-09 21:53:31 -0400

    added getCal Function, added a forEach to append data on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // once user opens deployed application they are presented with a decent UI
-// user inputs height, weight, age in first container .bmiInput
+// user inputs height, weight, age, gender, activity level in first container .bmiInput
 // press a .calcBmi button 
 // presented with bmi results in second container .bmiRes
 // user inputs activity, weight and duration in container .calInput
@@ -10,7 +10,9 @@ let inputEl1 = document.querySelector(".bmiInput");
 let bmiBtn = document.querySelector(".calcBmiBtn");
 // let bmiRes = document.querySelector(".");
 let inputEl2 = document.querySelector(".calInput");
-let calBurn = document.querySelector(".calBtn")
+let calBtn = document.getElementById("calBurnBtn");
+const activity = document.querySelector('#activity')
+let exceriseDiv = document.querySelector('#excerciseDiv')
 
 //Fetch bmiInput API
 //RESPONSE
@@ -24,6 +26,35 @@ let calBurn = document.querySelector(".calBtn")
 //DATA
 //PRESENT THE RESULTS FROM bmiInput on page
 //store it in local storage 
+
+async function getCal() {
+    const url = 'https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned?activity=' + activity.value ;
+    console.log(activity.value)
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dec3f09420msh8e059b9b79497f5p1aa256jsn405523c4c89c',
+		'X-RapidAPI-Host': 'calories-burned-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+    result.forEach(exercise => {
+        //create element 
+        let nameEl = document.createElement('p')
+        nameEl.textContent = exercise.name
+        exceriseDiv.append(nameEl)
+    });
+
+} catch (error) {
+	console.error(error);
+}
+}
+
+calBtn.addEventListener('click',getCal)
 
 //Localstorage
 //get and set item
